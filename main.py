917adbe2316b882553835769d55132a3cea0ea37
Rohith Teja M <rohithtejam@gmail.com>
2022-11-09 16:21:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,13 +26,14 @@ for i in [5,4,3,2,1]:
         temp = df.tail(i).Close.T.values
         X_test = np.append(temp, pred_list).reshape((1, 5, 1)).astype(float)
 
-        pred = model.predict(X_test)
+        pred = model.predict(X_test)[0]
         pred_list.append(pred)
 
 # show predictions
 st.write('Predictions for next 5 days:')
 pred = pd.DataFrame(pred_list, columns=['Price'])
 pred.index = pd.date_range(df.tail(1).index.values[0], periods=6, freq='D')[1:]
+pred.index = pd.to_datetime(pred.index)
 st.write(pred)
 
 # model performance
