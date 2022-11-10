355c5d9c63539fa595d32fee31e7251b372e6803
Rohith Teja M <rohithtejam@gmail.com>
2022-11-10 17:56:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,11 @@ def load_model():
     return model
 
 df = yf.download(tickers='BTC-USD', period = '6y', interval = '1d')
-update_metrics()
-retrain_model()
+
 # predict for next 7 days
 model = load_model()
 df2 = df.copy()
-for i in range(8):
+for i in range(91):
 
     X_test = df2.tail(5).Close.T.values.reshape((1, 5, 1))
     pred = model.predict(X_test)[0]
@@ -31,9 +30,9 @@ for i in range(8):
 
 # show predictions
 st.title('Bitcoin Price Prediction')
-st.write('Predictions for next 7 days:')
+st.write('Predictions for next 90 days:')
 pred = df2.copy()
-pred = pred.tail(7)
+pred = pred.tail(90)
 pred = pred.reset_index()
 pred = pred.rename(columns={'index': 'Date'})
 pred = pred[['Date', 'Close']]
@@ -42,7 +41,7 @@ st.write(pred)
 
 # plot current weeks price and forecast
 st.markdown('## Current week price and forecast')
-df3 = df.tail(7)
+df3 = df.tail(90)
 df3['Date'] = df3.index
 df3 = df3[['Date', 'Close']]
 df3 = df3.rename(columns={'Close': 'Predicted Price (USD)'})
@@ -50,7 +49,7 @@ df3.Date = pd.to_datetime(df3.Date)
 
 fig, ax = plt.subplots(figsize=(15,10))
 ax.plot(df3.Date, df3['Predicted Price (USD)'], label='Current week price')
-df4 = pd.concat([df3, pred]).tail(8)
+df4 = pd.concat([df3, pred]).tail(91)
 ax.plot(df4.Date, df4['Predicted Price (USD)'], label='Forecast')
 ax.legend()
 ax.set_ylabel('Price (USD)')
