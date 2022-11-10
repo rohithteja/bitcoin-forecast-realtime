e0b6eacb5c8c4712d9c28acde0775c7853c7748b
Rohith Teja M <rohithtejam@gmail.com>
2022-11-10 17:28:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,26 +21,23 @@ update_metrics()
 retrain_model()
 # predict for next 7 days
 model = load_model()
-pred_list = []
-for i in [7,6,5,4,3,2,1]:
-    if i == 7:
-        X_test = df.tail(i).Close.T.values.reshape((1, 3, 1))
-        pred = model.predict(X_test)[0]
-        pred_list.append(pred)
-    else:
-        temp = df.tail(i).Close.T.values
-        X_test = np.append(temp, pred_list).reshape((1, 3, 1)).astype(float)
+df2 = df.copy()
+for i in range(8):
 
-        pred = model.predict(X_test)[0]
-        pred_list.append(pred)
+    X_test = df2.tail(3).Close.T.values.reshape((1, 3, 1))
+    pred = model.predict(X_test)[0]
+    date = df2.tail(1).index[0] + pd.DateOffset(days=1)
+    df2 = pd.concat([df2, pd.DataFrame(data=pred, index=[date], columns=['Close'])])
 
 # show predictions
 st.title('Bitcoin Price Prediction')
 st.write('Predictions for next 7 days:')
-pred = pd.DataFrame(pd.date_range(df.tail(1).index.values[0], periods=8, freq='D')[1:], columns=['Date'])
-pred['Price (USD)'] = pred_list
-pred.Date = pd.to_datetime(pred.Date)
-pred['Price (USD)'] = pred['Price (USD)'].astype(int)
+pred = df2.copy()
+pred = pred.tail(7)
+pred = pred.reset_index()
+pred = pred.rename(columns={'index': 'Date'})
+pred = pred[['Date', 'Close']]
+pred = pred.rename(columns={'Close': 'Predicted Price (USD)'})
 st.write(pred)
 
 # plot current weeks price and forecast
