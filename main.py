400eec9fd025db3b22370caa0835173e6e05f5ba
Rohith Teja M <rohithtejam@gmail.com>
2022-11-10 16:49:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,20 +24,20 @@ model = load_model()
 pred_list = []
 for i in [7,6,5,4,3,2,1]:
     if i == 7:
-        X_test = df.tail(i).Close.T.values.reshape((1, 5, 1))
+        X_test = df.tail(i).Close.T.values.reshape((1, 7, 1))
         pred = model.predict(X_test)[0]
         pred_list.append(pred)
     else:
         temp = df.tail(i).Close.T.values
-        X_test = np.append(temp, pred_list).reshape((1, 5, 1)).astype(float)
+        X_test = np.append(temp, pred_list).reshape((1, 7, 1)).astype(float)
 
         pred = model.predict(X_test)[0]
         pred_list.append(pred)
 
 # show predictions
 st.title('Bitcoin Price Prediction')
-st.write('Predictions for next 5 days:')
-pred = pd.DataFrame(pd.date_range(df.tail(1).index.values[0], periods=6, freq='D')[1:], columns=['Date'])
+st.write('Predictions for next 7 days:')
+pred = pd.DataFrame(pd.date_range(df.tail(1).index.values[0], periods=8, freq='D')[1:], columns=['Date'])
 pred['Price (USD)'] = pred_list
 pred.Date = pd.to_datetime(pred.Date)
 pred['Price (USD)'] = pred['Price (USD)'].astype(int)
